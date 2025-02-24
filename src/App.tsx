4168def52dad6700886c5a6e2ad821989bc5e29c
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-02-24 10:31:22 +0200

    Finished Screen Component Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Screen from "./components/Screen";
 
 export default function App() {
   return (
     <div className="calculator">
       <Header></Header>
+      <Screen></Screen>
     </div>
   )
 }
---
+++ src/components/Screen.tsx
@@ -0,0 +1,8 @@
+
+export default function Screen() {
+  return (
+    <h1 className="screen">
+        399,981
+    </h1>
+  ) 
+}
